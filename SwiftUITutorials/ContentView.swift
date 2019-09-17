3ae858be16924958e490126d99922655a5250a84
Keval Shah <kjs503@nyu.edu>
2019-09-17 00:42:10 -0400

    Project creation

---
+++ SwiftUITutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorials
+//
+//  Created by Keval Shah on 9/17/19.
+//  Copyright © 2019 Keval Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
